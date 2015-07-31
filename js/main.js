0edf814f577a22fa573b76432a73e576d5bb876d
ingsantiago93 <spenuela@REN007.edistribution.loc>
2015-07-31 11:48:34 -0500

    Documentación
    Se documenta las acciones del código y se limpian "console.log"
    innecesarios

--- js/main.js
+++ js/main.js
@@ -1,32 +1,66 @@
+/* Santiago Peñuela Arcila - 31/07/2015 
+Evento Trigger que se ejecuta en la línea de tiempo principal de "_edgeActions.js" bajo el nombre "enviar"
+Función: recibe "evt" - evento que se ejecuta en "_edgeActions.js".
+Llama, mediante ajax, el archivo html para incrustarlo en el símbolo dentro del Edge, pues se creó un archivo nuevo (html)
+que se encarga de tener todas las acciones ".sortable" de jQuery UI y parametrización del "config.json".
+Posteriormente, llama el "config.json" para poblar la lista "ul#list_sort" con las palabras al azar, modificadas para
+que no siempre se muestren en el órden correcto. Al finalizar, le da el evento jQuery UI "sortable", para que los
+elementos de la lista se pueda arrastrar y acomodar cuando se suelta en algún lugar de la lista misma, además 
+guarda el "config.json" dentro de el simbolo principal y la cantidad de intentos con la que inicia el usuario. */
 $('body').on('enviar',function(evt)
-{    
-    //console.log(evt);    
+{   
+    var position_array = [];
+    var j, temp;
+
     $.ajax({url: "index.html", success: function(result){
         evt.sym.$('contenedor_padre').html(result);
             $.getJSON('config.json', function(json_content) {
-                //console.log(json_content);
                 $.each(json_content.words, function(pos, json_word) {
-                    /*console.log(pos);
-                    console.log(' >',
-                        'position: ' + pos,
-                        '/ word_symbol: ' + json_word.word,
-                        '/ space_symbol: ' + json_word.word,
-                        '/ word: ' + json_word.word
-                    );*/
-                    $("ul#list_sort li:nth-child(" + (pos + 1) + ")", evt.sym.$('contenedor_padre')).html(json_word.word);
-                    $("ul#list_sort", evt.sym.$('contenedor_padre')).sortable();
-                    $("ul#list_sort", evt.sym.$('contenedor_padre')).disableSelection();
+                    position_array[pos] = json_word.word;
                 });
+                for(var i = position_array.length - 1; i >= 0; i--)
+                {
+                    j = Math.floor(Math.random() * (i + 1));
+                    temp = position_array[i];
+                    position_array[i] = position_array[j];
+                    position_array[j] = temp;
+                    $("ul#list_sort li:nth-child(" +(i+1)+ ")", evt.sym.$('contenedor_padre')).html(position_array[i]);
+                }                
+                $("ul#list_sort", evt.sym.$('contenedor_padre')).sortable();
                 evt.sym.$("contenedor_padre").prop('ed_json_object',json_content);
                 evt.sym.$("contenedor_padre").prop('ed_user_attemps',0);
-            });            
-    }});    
+            });
+    }});
 });
+
+/*
+Evento Trigger que se ejecuta en la línea de tiempo principal de "_edgeActions.js" bajo el nombre "ed_check_list"
+Función: recibe "evt" - evento que se ejecuta en "_edgeActions.js". Se ejecuta cuando el usuario da click en el
+botón "revisar".
+Se crea la variable/función "check_order", la cual tiene los siguientes atributos:
+"retorno_datos": variable que contiene todos los datos que se van a imprimir como propiedad en el Edge para uso
+de SCORM.
+"activity_score": variable equivalente al puntaje que saca el usuario al realizar la actividad.
+"retorno_datos.user_answer": corresponde a un atributo de la variable "retorno datos" encargada de conservar
+en un arreglo, las respuestas del usuario.
+"json_content": captura el objeto json del Edge y lo conserva en dicha variable.
+
+Funciones:
+1. Captura cada una de las respuestas del usuario y las guarda en "retorno_datos.user_answer" para posteriormente, comparar
+con el JSON si las respuestas están bien o mal, además, de averiguar cual fue su puntaje en porcentaje.
+2. Revisa en el JSON cual es el puntaje MINIMO para que el usuario pueda pasar la actividad.
+3. Si el usuario pasa, busca la respectiva retroalimentación para mostrar y la adjunta al objeto "retorno_datos".
+4. Si el usuario no pasa, revisa cuantos intentos tiene pendientes, si tiene, regresa el juego a su estado "original",
+si no tiene, busca que tipo de retroalimentación "negativa" debe mostrar y la adjunta al objeto "retorno_datos". 
+
+Finalmente, retorna el objeto con los valores necesarios para el uso de la plantilla y del SCORM.
+*/
 
 $('body').on('ed_check_list', function(evt)
 {
     var check_order = function ()
     {
+        var position_array = [];
         var retorno_datos = {};
         var activity_score = 0;
         retorno_datos.user_answer = [];
@@ -36,8 +70,7 @@ $('body').on('ed_check_list', function(evt)
             if ($(this).html() == json_content.words[index].word) {
                 activity_score = activity_score + 1;
             } else {
-                //console.log(json_content.feedback.fdb_text);
-                //Do Nothing.
+                //No debe sumar puntos si su respuesta es incorrecta
             }
         });
 
@@ -49,13 +82,13 @@ $('body').on('ed_check_list', function(evt)
         retorno_datos.user_score = multiplicacion;
         retorno_datos.minimun_score = json_content.feedback.config_score.min_score_user;
 
+        //Revisar si el puntaje del usuario es suficiente para pasar.
         if(multiplicacion >= json_content.feedback.config_score.min_score_user)
         {
             $.each(json_content.feedback.correcto, function(pos, item)
             {
                 if(multiplicacion > parseInt(pos))
                 {
-                    console.log("entró");
                     retorno_datos.feedback = item;
                     return false;
                 }
@@ -67,7 +100,7 @@ $('body').on('ed_check_list', function(evt)
         }
         else
         {
-            //If de attemps.
+            //If de intentos.
             var intentos_usuario = evt.sym.$("contenedor_padre").prop('ed_user_attemps');
             if(typeof(json_content.feedback.attempts[intentos_usuario]) != "undefined")
             {
@@ -75,8 +108,16 @@ $('body').on('ed_check_list', function(evt)
                 intentos_usuario = intentos_usuario + 1;
                 evt.sym.$("contenedor_padre").prop('ed_user_attemps',intentos_usuario);
                 $.each(json_content.words, function(pos, json_word) {
-                    $("ul#list_sort li:nth-child(" + (pos + 1) + ")", evt.sym.$('contenedor_padre')).html(json_word.word);
+                    position_array[pos] = json_word.word;
                 });
+                for(var i = position_array.length - 1; i >= 0; i--)
+                {
+                    j = Math.floor(Math.random() * (i + 1));
+                    temp = position_array[i];
+                    position_array[i] = position_array[j];
+                    position_array[j] = temp;
+                    $("ul#list_sort li:nth-child(" +(i+1)+ ")", evt.sym.$('contenedor_padre')).html(position_array[i]);
+                }
             }
             else
             {
@@ -105,7 +146,9 @@ $('body').on('ed_check_list', function(evt)
     });
 });
 
+/*
+// Evento de prueba para verificar si está funcionando correctamente o no.
 $('body').on('Edge_Plantilla_Retroalimentacion', function(evt)
 {
     console.log(evt);
-});
+});*/
